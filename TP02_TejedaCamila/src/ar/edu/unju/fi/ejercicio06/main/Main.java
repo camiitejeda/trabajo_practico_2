ad9916f2e2e5b133a6af746229f01a07cddfcce0
Cami <Cami@LAPTOP-GRDN8A38>
2024-05-07 13:52:33 -0300

    Ejercicio06

--- TP02_TejedaCamila/src/ar/edu/unju/fi/ejercicio06/main/Main.java
+++ TP02_TejedaCamila/src/ar/edu/unju/fi/ejercicio06/main/Main.java
@@ -1,10 +1,29 @@
 package ar.edu.unju.fi.ejercicio06.main;
 
+import ar.edu.unju.fi.ejercicio06.interfaces.funcionales.*;
+import ar.edu.unju.fi.ejercicio06.model.*;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
-	}
+		FelinoDomestico gato = new FelinoDomestico("Garfield", (byte)45, 12f);
+		Converter<FelinoDomestico, FelinoSalvaje> converter = x -> new FelinoSalvaje(x.getNombre(), x.getEdad(), x.getPeso());
+		
+		FelinoSalvaje felino1 = converter.convert(gato);
+		converter.mostrarObjeto(felino1);
+		
+		FelinoSalvaje gatito = new FelinoSalvaje("Tanner", (byte)20, 186);
+		if(Converter.isNotNull(gatito))
+		{
+			Converter<FelinoSalvaje, FelinoDomestico> converter1 = x -> new FelinoDomestico(x.getNombre(), x.getEdad(), x.getPeso());
+			
+			FelinoDomestico gatito2 = converter1.convert(gatito);
+			converter1.mostrarObjeto(gatito2);
+		}
+		else
+		{
+			System.out.print("es nulo");
+		}
+		}
 
 }
